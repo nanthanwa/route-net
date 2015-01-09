c32f63a70c29973a17f3f16999e55df0c63e65fe
Thanwa Nooploy <duodecimus@hotmail.com>
2015-01-09 15:06:22 +0700

    edit controller device

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -9,26 +9,12 @@ angular.module('starter.controllers', [])
    
     
   $scope.goMap= function(params){
+    console.log("route-net : goMap()");
     console.log("BUS :"+ params.domainbus)
     console.log("CAR :"+ params.domaincar)
     DomainsService.set(params);
     $location.path('/map');
   }
-
-})
-
-
-
-.controller('MapCtrl', function($scope, $ionicLoading, $location, DomainsService) {
-
-  $scope.device = "";
-
-
-  $scope.bus=DomainsService.get();
-  console.log($scope.bus);
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
 
   // Wait for device API libraries to load
     //
@@ -46,6 +32,22 @@ angular.module('starter.controllers', [])
                             'Device Version: '  + device.version  + '<br />';
     }
 
+})
+
+
+
+.controller('MapCtrl', function($scope, $ionicLoading, $location, DomainsService) {
+
+  $scope.device = "";
+
+
+  $scope.bus=DomainsService.get();
+  console.log($scope.bus);
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  
   $scope.centerOnMe = function () {
     console.log("Centering");
     if (!$scope.map) {
@@ -72,25 +74,9 @@ angular.module('starter.controllers', [])
   //Controller for DOMAIN !!
   $scope.goDomain= function(){
 
-    console.log("route-net : GODOMAIN");
+    console.log("route-net : goDomain()");
 
     $location.path('/domain');
   }
-  
-  // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready", onDeviceReady, false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        var element = document.getElementById('deviceProperties');
-        element.innerHTML = 'Device Name: '     + device.name     + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Model: '    + device.model    + '<br />' +
-                            'Device Version: '  + device.version  + '<br />';
-    }
 
 });
