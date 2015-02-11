690a58a4ce87653cef046ad3e99373bc7bc5ebc2
Thanwa Nooploy <duodecimus@hotmail.com>
2015-02-11 21:10:47 +0700

    no message

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -15,22 +15,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
 
 .config(function($stateProvider, $urlRouterProvider) {
   
-
   $stateProvider
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-  })
-  .state('domain', {
-    url: "/domain",
-    templateUrl: "templates/domain.html",
-    controllers: "DomainCtrl"
+  .state('home', {
+    url: "/home",
+    templateUrl: "templates/home.html",
+    controllers: "HomeCtrl"
   })
   .state('map', {
     url: "/map",
     templateUrl: "templates/map.html",
     controllers: "MapCtrl"
   })
-  $urlRouterProvider.otherwise("/domain");
+  .state('profile', {
+    url: "/profile",
+    templateUrl: "templates/profile.html",
+    controllers: "ProfileCtrl"
+  })
+  $urlRouterProvider.otherwise("/home");
 })
